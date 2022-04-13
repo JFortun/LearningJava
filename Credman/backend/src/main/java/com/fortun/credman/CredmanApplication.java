cf0159c9e258f32889d52e91f91f653a54d3d970
JFortun <josemanuelfortunromero@gmail.com>
2022-04-13 20:37:09 +0200

    Initial Credman backend project

---
+++ Credman/backend/src/main/java/com/fortun/credman/CredmanApplication.java
@@ -0,0 +1,13 @@
+package com.fortun.credman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ Credman/backend/src/test/java/com/fortun/credman/CredmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortun.credman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
