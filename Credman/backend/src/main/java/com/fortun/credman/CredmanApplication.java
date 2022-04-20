addd2b2df7895ba9f58932c0627c41db8e6920c8
JFortun <josemanuelfortunromero@gmail.com>
2022-04-20 04:34:51 +0200

    Database configuration

--- Credman/backend/src/main/java/com/fortun/credman/CredmanApplication.java
+++ Credman/backend/src/main/java/com/fortun/credman/CredmanApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CredmanApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(CredmanApplication.class, args);
 	}
 
 }
